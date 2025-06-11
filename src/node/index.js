541ab2a0d198b17c93c359fc232e313afcce617a
renta0525 <renzu525@gmail.com>
2025-06-11 14:15:15 +0900

    28km

--- src/node/index.js
+++ src/node/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require('cors');
 const { Pool } = require("pg");
+const path = require("path");
 
 const app = express();
 const port = 4754;
@@ -10,6 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 app.use(express.static("public"));
+app.use('/renta_ueno', express.static(path.join(__dirname, '../web')));
 
 // DB接続
 const pool = new Pool({
